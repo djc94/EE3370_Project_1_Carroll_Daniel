d095800446014a9ebd29f72acc3c6b4d709ff7cd
djc94 <djc94@txstate.edu>
2015-02-26 19:59:03 -0600

    Fully Edited MatlabCode1.m
    Fully Edited MatlabCode1.m

--- MatlabCode1.m
+++ MatlabCode1.m
@@ -1,10 +1,10 @@
 % EE3370 Project 1
-% MATLAB code for x_t=3*cos(omega*t+angle);
+% MATLAB code for x_t*u_t;
 % Daniel Carroll
 % 2/26/2015
 
 
-% I could not commit the MATLAB file using the SVN client (SmartSVN) on my Mac. 
+% I could not commit the MATLAB file using my SVN client (SmartSVN) on my Mac. 
 % I received the error: "Invalid PROPPATCH properties"
 
 % Instead i am committing this file directly through github.com.  
@@ -12,14 +12,23 @@
 
 clc      % Clear screen 
 clear    % Clear all variables
+
 A = 3; f= 1000; omega = 2*pi*f; 
-angle = degtorad(33); T=1/f;
-t=linspace(-3*T,3*T,1000);
+angle = (33*3.1415952)/180; T=1/f;
+t=linspace(-3000*T,3000*T,1000);
+
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+f_t=cos(omega*t).*sin(omega*t).*heaviside(t-1)
+
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+
+subplot(4,1,4)
+plot(t,f_t, 'linewidth',2);grid on
